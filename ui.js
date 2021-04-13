a7f37abb80386d9da276d2b686801e0e0d94215c
Adam Malíř <astromalir@gmail.com>
2021-04-13 20:47:31 +0200

    ui improved

--- ui.js
+++ ui.js
@@ -1,43 +1,55 @@
-let canvas = document.getElementById('cnvs');
-let ctx = canvas.getContext('2d');
 
-let gridSett = {
-    colls: 100,
-    rows: 150,
-    pxSize: 10,
-    gap: 2,
+function render() {
+
+	let pxSize = gridSett.pxSize;
+
+	for(let x=0; x < grid[0].length; x++){
+		for(let y=0; y < grid.length; y++){
+			if(grid[y][x] === 1){
+				ctx.fillStyle = 'black';
+				ctx.fillRect(
+					x*(pxSize+gridSett.gap) + pxSize, 
+					y*(pxSize+gridSett.gap) + pxSize, 
+					pxSize, pxSize);
+			}
+		}
+	}
 }
 
 
 function create2DArray(colls, rows) {
-let arr = new Array(colls)
-    for (let i = 0; i < colls; i++){
-        arr[i] = new Array(rows)
-    }
-    return arr;
+	let arr = new Array(colls)
+
+	for (let i = 0; i < colls; i++)
+		arr[i] = new Array(rows)
+	return arr;
 }
+
+
+
+let canvas = document.getElementById('cnvs');
+let ctx = canvas.getContext('2d');
+
+let gridSett = {
+	pxSize: 10,
+	gap: 2,
+}
+
+gridSett.rows = Math.floor(canvas.height/(gridSett.pxSize+gridSett.gap));
+gridSett.colls = Math.floor(canvas.width/(gridSett.pxSize+gridSett.gap));
+
+console.log(gridSett.rows);
+console.log(gridSett.colls);
 
 let grid = create2DArray(gridSett.colls, gridSett.rows);
 
-//add some data to grid array
-grid[1][5] = 1;
-grid[2][5] = 1;
-grid[1][6] = 1;
 
-function render() {
-    for(let y=0; y < grid.length; y++){
-        for(let x=0; x < grid[y].length; x++){
-            if(grid[y][x] === 1){
-                ctx.fillStyle = 'black';
-                let pxSize = gridSett.pxSize;
-                ctx.fillRect(
-                    x*(pxSize+gridSett.gap) + pxSize, 
-                    y*(pxSize+gridSett.gap) + pxSize, 
-                    pxSize, pxSize);
-            }
-        }
-    }
-}
+
+
+//add some data to grid array
+grid[0][0] = 1;
+grid[1][0] = 1;
+grid[30][48] = 1;
 
 render();
 
