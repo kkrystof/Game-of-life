ac8347c116dfc770a4d418f695b67eedfb5679f0
Adam Malíř <astromalir@gmail.com>
2021-04-15 23:45:16 +0200

    responding grid on window resize or on change 'gridSett.n' (supposed to use in the future), refactoring

--- ui.js
+++ ui.js
@@ -1,56 +1,58 @@
 
+let   canvas = 	document.getElementById('cnvs'),
+      ctx    = 	canvas.getContext('2d'),
+      gridSett = {
+        n: 30,
+        d: canvas.width,
+        liveCells: [],
+        size: 0,
+        gap: 0,
+        r: 15 // ratio size = r*gap
+	};
+
+window.onresize = function() {resizeGrid()};
+
+
 function renderLiveCells() {
 
 	let shift =
-		(gridSett.gap + ((gridSett.d/(gridSett.gap+gridSett.pxSize))%1)*(gridSett.gap+gridSett.pxSize))/gridSett.n;
+		(gridSett.gap + ((gridSett.d/(gridSett.gap+gridSett.size))%1)*(gridSett.gap+gridSett.size))/gridSett.n;
 
 	// the first (0) rect doesn't add the shift .. 0*( .. and therefore  
 	shift += shift/(gridSett.n-1);
 
-	let pxSize = gridSett.pxSize;
+	let size = gridSett.size;
 	ctx.fillStyle = 'black';
 
 	for(let i = 0; i < gridSett.liveCells.length; i++)
 		ctx.fillRect(
-			gridSett.liveCells[i][0]*(pxSize+gridSett.gap+shift),
-			gridSett.liveCells[i][1]*(pxSize+gridSett.gap+shift),
-			pxSize, pxSize);
+			gridSett.liveCells[i][0]*(size+gridSett.gap+shift),
+			gridSett.liveCells[i][1]*(size+gridSett.gap+shift),
+			size, size);
+}
+
+function resizeGrid() {
+	canvas.height = Math.min(window.innerHeight,window.innerWidth);
+	canvas.width = canvas.height;
+
+	gridSett.d = canvas.height;
+	gridSett.size = gridSett.r * gridSett.d/(gridSett.n*(gridSett.r+1));
+	gridSett.gap = gridSett.size/gridSett.r;
+	renderLiveCells();	
+}
+
+function testAddSomeCells() {
+	gridSett.liveCells.push(new Array(0,0));
+	gridSett.liveCells.push(new Array(0,1));
+	gridSett.liveCells.push(new Array(1,1));
+	gridSett.liveCells.push(new Array(27,10));
+	gridSett.liveCells.push(new Array(gridSett.n-1,gridSett.n-1));
+	gridSett.liveCells.push(new Array(gridSett.n-1,gridSett.n-1));
+	gridSett.liveCells.push(new Array(gridSett.n-1,1));
 }
 
 
+testAddSomeCells();
+resizeGrid();
 
-document.getElementById('cnvs').width= Math.min(screen.availWidth,screen.availHeight);
-document.getElementById('cnvs').height = document.getElementById('cnvs').width; 
-
-
-let canvas = document.getElementById('cnvs');
-let ctx = canvas.getContext('2d');
-let gridSett = {
-	n: 10,
-	d: document.getElementById('cnvs').width,
-	liveCells: [],
-	pxSize: 0,
-	gap: 0,
-	r: 8 // ratio pxSize = r*gap
-}
-
-
-gridSett.pxSize = gridSett.r * gridSett.d/(gridSett.n*(gridSett.r+1));
-gridSett.gap = gridSett.pxSize/gridSett.r;
-
-
-
-
-
-//add some cells to grid 
-gridSett.liveCells.push(new Array(0,0));
-gridSett.liveCells.push(new Array(0,1));
-gridSett.liveCells.push(new Array(1,1));
-gridSett.liveCells.push(new Array(27,10));
-gridSett.liveCells.push(new Array(gridSett.n-1,gridSett.n-1));
-gridSett.liveCells.push(new Array(gridSett.n-1,gridSett.n-1));
-gridSett.liveCells.push(new Array(gridSett.n-1,1));
-
-
-renderLiveCells();
 
